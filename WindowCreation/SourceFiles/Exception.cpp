0a9c385ec23e9dfa086d415aff69727d0f0343ce
Miquel <miguel.nasarre.budino@gmail.com>
2023-10-15 22:49:54 +0200

    Lots of stuff added, now theres a better window i guess xd

---
+++ WindowCreation/SourceFiles/Exception.cpp
@@ -0,0 +1,40 @@
+#include "Exception.h"
+
+ExceptionClass::ExceptionClass( int line,const char* file ) noexcept
+	:line( line ),
+	file( file )
+{
+
+}
+
+const char* ExceptionClass::what() const noexcept
+{
+	std::ostringstream oss;
+	oss << GetType() << std::endl
+		<< GetOriginString();
+	whatBuffer = oss.str();
+	return whatBuffer.c_str();
+}
+
+const char* ExceptionClass::GetType() const noexcept
+{
+	return "Chili Exception";
+}
+
+int ExceptionClass::GetLine() const noexcept
+{
+	return line;
+}
+
+const std::string& ExceptionClass::GetFile() const noexcept
+{
+	return file;
+}
+
+std::string ExceptionClass::GetOriginString() const noexcept
+{
+	std::ostringstream oss;
+	oss << "[File] " << file << std::endl
+		<< "[Line] " << line;
+	return oss.str();
+}
--- WindowCreation/SourceFiles/WinMain.cpp
+++ WindowCreation/SourceFiles/WinMain.cpp
@@ -1,73 +1,25 @@
-#include <Windows.h>
-#include <WinUser.h>
-
-//	Window procedure with inputs
-//	https://learn.microsoft.com/en-us/windows/win32/learnwin32/writing-the-window-procedure
-//
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-	switch (uMsg)
-	{
-	case(WM_CLOSE):
-		PostQuitMessage(69);
-		break;
-	}
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
-}
+#include "Header.h"
+#include "Window.h"
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	//	Register Window class
-	//	https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-wndclassexa
-	//
+	// Create a window
 
-	WNDCLASSEX wc = { 0 };
-	const LPCWSTR ClassName = L"Hello World";
-
-	wc.cbSize			= sizeof(wc);
-	wc.style			= CS_OWNDC;
-	wc.lpfnWndProc		= WindowProc;
-	wc.cbClsExtra		= 0;
-	wc.cbWndExtra		= 0;
-	wc.hInstance		= hInstance;
-	wc.hIcon			= NULL;
-	wc.hIconSm			= NULL;
-	wc.hCursor			= NULL;
-	wc.hbrBackground	= NULL;
-	wc.lpszMenuName		= NULL;
-	wc.lpszClassName	= ClassName;
-
-	RegisterClassEx(&wc);
-
-	//	Create Window
-	//	https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexa
-	//
-
-	HWND hWnd = CreateWindowEx(
-		0,
-		ClassName,
-		L"Just a window",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_SYSMENU | WS_VISIBLE,
-		200,
-		200,
-		640,
-		480,
-		NULL,
-		NULL,
-		hInstance,
-		NULL
-	);
+	Window window(640, 480, L"Hello World");
 
 	//	Message pump
-	//	https://learn.microsoft.com/es-es/windows/win32/api/winuser/nf-winuser-getmessage
-	//
-
+	
 	MSG msg;
-	while (GetMessage(&msg, hWnd, 0, 0) > 0) {
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0) {
 		TranslateMessage(&msg);
-		DispatchMessageW(&msg);
+		DispatchMessage(&msg);
 	}
+
+	//	Check if GetMessage call itself borked
+
+	if (gResult == -1)
+		return -1;
 
 	return (int)msg.wParam;
 }
